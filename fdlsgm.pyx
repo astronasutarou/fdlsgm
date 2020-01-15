046acb7b27b3bc7cfd840e83287f2c667e5412c4
Ryou Ohsawa <ohsawa@ioa.s.u-tokyo.ac.jp>
2020-01-15 17:55:09 +0900

    update sample script

--- fdlsgm.pyx
+++ fdlsgm.pyx
@@ -1,8 +1,17 @@
-#!/usr/bin/env python
-from libc.math cimport sin
-from libcpp.vector cimport vector
-from libcpp.list cimport list
+#!/usr/bin/env cython
+# -*- coding: utf-8 -*-
+from libcpp.list cimport list as clist
+import numpy as np
 
 
-cdef double f (double x):
-  return sin(x * x)
+cdef clist[int] count_number(int N):
+  cdef int n = 0
+  cdef clist[int] ret
+  for n in range(N):
+    ret.push_back(n)
+  return ret
+
+def count(int N):
+  cdef clist[int] aaa = count_number(N)
+  for x in aaa:
+    print(x)
