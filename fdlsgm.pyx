4e02ca0998c51d98a198798817d5742ac09100cd
Ryou Ohsawa <ohsawa@ioa.s.u-tokyo.ac.jp>
2020-01-23 18:23:07 +0900

    Cython script updated

--- fdlsgm.pyx
+++ fdlsgm.pyx
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from libcpp.vector cimport vector as vec
 from libcpp.list cimport list as clist
+from numpy cimport ndarray
 import numpy as np
 
 
@@ -17,100 +18,70 @@ cdef extern from 'fdlsgm.h' namespace 'fdlsgm':
     dls(const double&, const double&, const double&,
         const double&, const double&, const double&) except+
 
-  cdef cppclass baseline:
-    double x0()
-    double y0()
-    double z0()
-    double x1()
-    double y1()
-    double z1()
-    int size()
-    void dprint()
-    clist[long] elements()
+  ctypedef struct baseline_view:
+    double v0[3]
+    double v1[3]
+    int size
+    int* elements
 
-  cdef cppclass accumulator[N]:
-    accumulator() except+
-    void insert(const dls&, const parameter&)
-    void reallocate(const parameter&)
-    void coalesce(const parameter&)
-    int count_baseline()
-    int count_segment()
-    const baseline& operator[]()
-
-  cdef const vec[baseline] find_segments(
-    const vec[dls]& pool, const size_t& size_threshold)
+  cdef const vec[baseline_view] find_segments(
+    const size_t& n_size, const double* pool, const size_t& size_threshold)
 
 
 cdef extern from *:
   ctypedef int N360 '360'
 
 
-class solver(object):
-  def __init__(self, limit=6):
-    self.pool = list()
-    self.limit = limit
-
-  def insert(self, double x1, double y1, double z1,
-             double x2, double y2, double z2):
-    self.pool.append((x1,y1,z1,x2,y2,z2))
-
-  def calculate(self):
-    cdef vec[dls] pool
-    cdef vec[baseline] result
-    cdef clist[long] elem
-    for x1,y1,z1,x2,y2,z2 in self.pool:
-      pool.push_back(dls(x1,y1,z1,x2,y2,z2))
-    result = find_segments(pool=pool, size_threshold=self.limit)
-    baseline = list()
-    bvector = list()
-    for n in range(result.size()):
-      elem = result[n].elements()
-      baseline.append([e for e in elem])
-      bvector.append(
-        (result[n].x0(),result[n].y0(),result[n].z0(),
-         result[n].x1(),result[n].y1(),result[n].z1()))
-    return baseline, bvector
+cdef view2dict(baseline_view v):
+  cdef int n = v.size
+  cdef list e = [v.elements[i] for i in range(n)]
+  return dict(v0=v.v0, v1=v.v1, n=n, e=e)
 
 
-def simple_solver_test(int group=10, int frame=10, int scatter=1000):
-  cdef int m, n
+def solve(ndarray pool, const int limit = 6):
+  if isinstance(pool,np.ndarray):
+    pool = pool.flatten()
+  cdef vec[baseline_view] result
+  cdef const double[:] view = pool
+  cdef size_t n_size = len(pool)//6
+  result = find_segments(n_size=n_size, pool=&view[0], size_threshold=limit)
+  baseline = list()
+  for n in range(result.size()):
+    baseline.append(view2dict(result[n]))
+  return baseline
+
+
+def simple_solver_test(
+    int group=10, int frame=10, int jammer=100, double scatter=0.05):
+  cdef int n
   cdef double x0,y0,z0,x1,y1,z1,x2,y2,z2,vx,vy,vz
+  cdef list baseline
   from datetime import datetime
   segments = list()
-  s = solver()
-  for m in range(group):
-    x0 = np.random.uniform(-100,100)
-    y0 = np.random.uniform(-100,100)
-    z0 = np.random.uniform(-100,100)
-    vx = np.random.normal()
-    vy = np.random.normal()
-    vz = np.random.normal()
-    for n in range(frame):
-      x1 = x0+vx*n+np.random.normal()/10.
-      y1 = y0+vy*n+np.random.normal()/10.
-      z1 = z0+vz*n+np.random.normal()/10.
-      x2 = x0+vx*(n+1)+np.random.normal()/10.
-      y2 = y0+vy*(n+1)+np.random.normal()/10.
-      z2 = z0+vz*(n+1)+np.random.normal()/10.
-      segments.append((m,x1,y1,z1,x2,y2,z2))
-      s.insert(x1,y1,z1,x2,y2,z2)
-  for m in range(scatter):
-    x1 = np.random.uniform(-100,100)
-    y1 = np.random.uniform(-100,100)
-    z1 = np.random.uniform(-100,100)
-    x2 = x1+np.random.normal()
-    y2 = y1+np.random.normal()
-    z2 = z1+np.random.normal()
-    segments.append((-1,x1,y1,z1,x2,y2,z2))
-    s.insert(x1,y1,z1,x2,y2,z2)
+  shape = (frame,3)
+  for n in range(group):
+    p0 = np.random.uniform(-100,100, size=(1,3))
+    v0 = np.random.normal(size=(1,3))
+    v = np.vstack((np.zeros((1,3)),np.cumsum(v0*np.ones((frame,3)),axis=0)))
+    p = np.hstack((p0+v[0:frame],p0+v[1:frame+1]))
+    p = p + np.random.normal(0.0, scatter, size=p.shape)
+    segments.extend(p)
+  p0 = np.random.uniform(-100,100, size=(jammer,3))
+  p1 = p0 + np.random.normal(size=(jammer,3))
+  p = np.hstack((p0,p1))
+  segments.extend(p)
+
+  segments = np.array(segments)
 
   t0 = datetime.now()
-  elem,vector = s.calculate()
+  baseline = solve(segments)
   t1 = datetime.now()
   print('## elements')
   for s in segments:
-    print('{} {} {} {} {} {} {}'.format(*s))
+    print('{} {} {} {} {} {}'.format(*s))
   print('\n\n## baselines')
-  for n,v in enumerate(vector):
-    print('{} {} {} {} {} {} {}'.format(n,*v))
-  print('\n# elapsed time: {}ms'.format((t1-t0).microseconds/1e3))
+  for n,b in enumerate(baseline):
+    print('{} {} {} {} {} {}'.format(*b['v0'],*b['v1']))
+  print('')
+  print('# found {} segments'.format(len(baseline)))
+  print('# elapsed time: {}ms'.format((t1-t0).microseconds/1e3))
